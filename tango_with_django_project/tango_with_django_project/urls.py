d61152fb4cc2686897b541f7d73d480545da4142
Nick Chemsak <nchemsak@gmail.com>
2017-01-22 16:43:00 -0600

    added about view

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.about, name='about'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting
     # with rango/ to be handled by
